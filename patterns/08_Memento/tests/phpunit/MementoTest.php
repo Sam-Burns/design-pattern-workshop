61e409fd1edeab9751b786e61461d6fe7e0f9213
Sam-Burns <sam.burns.uk@gmail.com>
2015-07-25 23:51:30 +0100

    Adding integration tests for Memento Pattern

---
+++ patterns/08_Memento/tests/phpunit/MementoTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace DesignPatterns\Memento\Test;
+
+use DesignPatterns\Memento\Counter;
+use DesignPatterns\Memento\Counter\Caretaker;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class MementoTest extends TestCase
+{
+    public function testMementoPattern()
+    {
+        $this->markTestSkipped('Memento pattern not implemented yet');
+
+        // ARRANGE
+        $caretaker = new Caretaker();
+        $counter = new Counter();
+
+        // ACT
+        $results = $caretaker->doSomeCounting($counter);
+
+        // ASSERT
+        $this->assertEquals(
+            [1, 2, 3, 4, 5, 4, 5],
+            $results
+        );
+    }
+}
