bd99cabb82c8c7bb62eecbfe766b7ed11bc1de42
Sam-Burns <sam.burns.uk@gmail.com>
2015-07-25 23:28:04 +0100

    Adding tests for additional Decorator Pattern example

--- patterns/07_Decorator/tests/phpunit/DecoratorTest.php
+++ patterns/07_Decorator/tests/phpunit/DecoratorTest.php
@@ -7,20 +7,53 @@ use PHPUnit_Framework_TestCase as TestCase;
 
 class DecoratorTest extends TestCase
 {
+    /** @var DataGetterBuilder */
+    private $dataGetterBuilder;
+
+    public function setUp()
+    {
+        $this->dataGetterBuilder = new DataGetterBuilder();
+    }
+
     public function testDecoratorPatternClassesArrangedCorrectly()
     {
         $this->markTestSkipped('Decorator Pattern not implemented yet');
 
         // ARRANGE
-        $dataGetterBuilder = new DataGetterBuilder();
-        $dataGetter = $dataGetterBuilder->buildDecoratorPatternObjectGraph();
+        $dataGetter = $this->dataGetterBuilder->buildDecoratorPatternObjectGraph();
 
         // ACT
-        $result1 = $dataGetter->getNameOfDesignPattern();
-        $result2 = $dataGetter->getNameOfDesignPattern();
+        $results = $this->getResultsOfFirstAndSecondCall($dataGetter);
 
         // ASSERT
-        $this->assertEquals('decorator',                      $result1);
-        $this->assertEquals('decorator <- Came from cache!!', $result2);
+        $this->assertEquals('decorator',                      $results[0]);
+        $this->assertEquals('decorator <- Came from cache!!', $results[1]);
+    }
+
+    public function testServiceWithNoCacheDecorator()
+    {
+        $this->markTestSkipped('Decorator Pattern not implemented yet');
+
+        // ARRANGE
+        $dataGetter = $this->dataGetterBuilder->buildObjectGraphWithNoCacheDecorator();
+
+        // ACT
+        $results = $this->getResultsOfFirstAndSecondCall($dataGetter);
+
+        // ASSERT
+        $this->assertEquals('decorator', $results[0]);
+        $this->assertEquals('decorator', $results[1]);
+    }
+
+    /**
+     * @param DataGetter $dataGetter
+     * @return string[]
+     */
+    private function getResultsOfFirstAndSecondCall(DataGetter $dataGetter)
+    {
+        return [
+            $dataGetter->getNameOfDesignPattern(),
+            $dataGetter->getNameOfDesignPattern()
+        ];
     }
 }
