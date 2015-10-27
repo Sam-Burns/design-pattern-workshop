49adf638c278427dcb2866ca3167ff9e5ce5f24a
Riya Dennis <riya.dennis@sainsburys.co.uk>
2015-10-27 15:50:05 +0000

    Adding dependency injection

--- patterns/03_Dependency_Injection/tests/phpunit/DependencyInjectionTest.php
+++ patterns/03_Dependency_Injection/tests/phpunit/DependencyInjectionTest.php
@@ -1,6 +1,10 @@
 <?php
 namespace DesignPatterns\DependencyInjection\Test;
 
+use DesignPatterns\DependencyInjection\DependencyManagementApproach\ConstructorInjection;
+use DesignPatterns\DependencyInjection\DependencyManagementApproach\InlineInstantiation;
+use DesignPatterns\DependencyInjection\DependencyManagementApproach\InlineSingletonAccess;
+use DesignPatterns\DependencyInjection\DependencyManagementApproach\SetterInjection;
 use PHPUnit_Framework_TestCase as TestCase;
 use PHPUnit_Framework_MockObject_MockObject as Mock;
 use DesignPatterns\DependencyInjection\DependencyWithNastySideEffects;
@@ -19,57 +23,53 @@ class DependencyInjectionTest extends TestCase
 
     public function testClassWithConstructorInjection()
     {
-        // @todo If you can...
+        $object = new ConstructorInjection($this->mockDependency);
 
-        $this->markTestSkipped('DI exercise not implemented');
 
-//        $this->assertInstanceOf(
-//            '\DesignPatterns\DependencyInjection\DependencyManagementApproach\ConstructorInjection',
-//            $object
-//        );
-//
-//        $this->assertTrue($object->hasMockDependency());
+        $this->assertInstanceOf(
+            '\DesignPatterns\DependencyInjection\DependencyManagementApproach\ConstructorInjection',
+            $object
+        );
+
+        $this->assertTrue($object->hasMockDependency());
     }
 
     public function testClassWithSetterInjection()
     {
-        // @todo If you can...
+        $object = new SetterInjection();
+        $object->setDependency($this->mockDependency);
 
-        $this->markTestSkipped('DI exercise not implemented');
+        $this->assertInstanceOf(
+            '\DesignPatterns\DependencyInjection\DependencyManagementApproach\SetterInjection',
+            $object
+        );
 
-//        $this->assertInstanceOf(
-//            '\DesignPatterns\DependencyInjection\DependencyManagementApproach\SetterInjection',
-//            $object
-//        );
-//
-//        $this->assertTrue($object->hasMockDependency());
+        $this->assertTrue($object->hasMockDependency());
     }
 
     public function testClassWithInlineInstantiation()
     {
-        // @todo If you can...
-
         $this->markTestSkipped('DI exercise not implemented');
 
-//        $this->assertInstanceOf(
-//            '\DesignPatterns\DependencyInjection\DependencyManagementApproach\InlineInstantiation',
-//            $object
-//        );
-//
-//        $this->assertTrue($object->hasMockDependency());
+        $object = new InlineInstantiation();
+        $object->retrieveDependencies();
+        $this->assertInstanceOf(
+            '\DesignPatterns\DependencyInjection\DependencyManagementApproach\InlineInstantiation',
+            $object
+        );
+
+        $this->assertTrue($object->hasMockDependency());
     }
 
     public function testClassWithInlineSingletonAccess()
     {
-        // @todo If you can...
-
+        $object = new InlineSingletonAccess();
+        $this->assertInstanceOf(
+            '\DesignPatterns\DependencyInjection\DependencyManagementApproach\InlineSingletonAccess',
+            $object
+        );
         $this->markTestSkipped('DI exercise not implemented');
 
-//        $this->assertInstanceOf(
-//            '\DesignPatterns\DependencyInjection\DependencyManagementApproach\InlineSingletonAccess',
-//            $object
-//        );
-//
-//        $this->assertTrue($object->hasMockDependency());
+        $this->assertTrue($object->hasMockDependency());
     }
 }
