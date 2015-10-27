0fcb8fc920e792dafc82e4d7940ac29eb5eba74d
Riya Dennis <riya.dennis@sainsburys.co.uk>
2015-10-27 15:29:48 +0000

    Adding bulider pattern code to pass the test

--- patterns/02_Builder/src/PubBuilder.php
+++ patterns/02_Builder/src/PubBuilder.php
@@ -14,6 +14,8 @@ class PubBuilder
      */
     public function buildPub(Staff $staff, $numberOfTables, PubSign $pubSign)
     {
-        // @todo
+       $pub = new Pub($numberOfTables, $staff->getBarman());
+        $pub->setPubsign($pubSign);
+        return $pub;
     }
 }
